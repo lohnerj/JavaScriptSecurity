2fd0f7822cea45cbc996aae34932ddc3cad3bc17
Josh Lohner <lohnerjosh@gmail.com>
2023-05-03 03:15:38 -0400

    Documentation

--- makepasswd.js
+++ makepasswd.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const crypto = require('crypto');
-
+encryptPassword()
 function encryptPassword() {
 // Read from password.txt
     const passwordData = fs.readFileSync('password.txt', 'utf-8');
